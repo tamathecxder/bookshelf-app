eb5176a1f0eb4dbc9f907aba20b488f08bfab929
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-07-29 22:27:32 +0700

    Completed to input and show bookshelf data, including make a function, styling ~whatever

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,11 +15,60 @@
 
 // Initialize Important Variables
 const books = [];
-const WEB_STORAGE_KEY = 'BOOKSHELF_APPS';
-const CUSTOM_SAVED_EVENT = 'saved-bookshelf';
-const CUSTON_RENDER_EVENT = 'render-bookshelf'
+const WEB_STORAGE_KEY = "BOOKSHELF_APPS";
+const CUSTOM_SAVED_EVENT = "saved-bookshelf";
+const CUSTOM_RENDER_EVENT = "render-bookshelf";
 
-const btnChecker = document.querySelectorAll('.button-checker');
+/**
+ * Modal Section
+ */
+
+const modal = document.getElementById("myModalElement");
+const button = document.getElementById("myModalButton");
+const closeModal = document.getElementById("closeModal");
+
+// ketika user click button nya, maka modal akan terbuka
+button.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+// ketika user click pada closeModal, maka modal nya akan tertutup
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// ketika user click dimanapun diluar modal, maka modal juga akan tertutup
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+/**
+ * Nav and Tabs section
+ */
+function openTabs(evt, sectionName) {
+  let i;
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  const tablinks = document.getElementsByClassName("tablinks");
+  const sectionTarget = document.getElementById(sectionName);
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  
+  sectionTarget.style.display = "grid";
+  sectionTarget.style.justifyItems = "space-evenly";
+  sectionTarget.style.gridAutoFlow = "column";
+  sectionTarget.style.gridTemplateColumns = "1fr, 1fr";
+  sectionTarget.style.gap = "20px";
+
+  evt.currentTarget.className += " active";
+}
 
 function generateId() {
   return +new Date();
@@ -27,17 +76,21 @@ function generateId() {
 
 function generateBookObject(id, title, author, year, isComplete) {
   return {
-    id, title, author, year, isComplete
+    id,
+    title,
+    author,
+    year,
+    isComplete,
   };
 }
 
 function isWebStorageExist() {
-  if (typeof (Storage) === undefined) {
+  if (typeof Storage === undefined) {
     alert("Your browser does not support the web save feature");
     return false;
   }
 
-  return true; 
+  return true;
 }
 
 function saveDataToStorage() {
@@ -49,16 +102,16 @@ function saveDataToStorage() {
 }
 
 function addBookList() {
-  const titleInput = document.getElementById('title').value;
-  const authorInput = document.getElementById('author').value;
-  const yearInput = document.getElementById('year').value;
-  const checkbox = document.getElementById('completed-checkbox').value;
+  const titleInput = document.getElementById("title").value;
+  const authorInput = document.getElementById("author").value;
+  const yearInput = document.getElementById("year").value;
+  const checkbox = document.getElementById("completed-checkbox").value;
 
   const generatedRandomID = generateId();
   const bookObject = generateBookObject(generatedRandomID, titleInput, authorInput, yearInput, checkbox);
   books.unshift(bookObject);
 
-  document.dispatchEvent(new Event(CUSTON_RENDER_EVENT));
+  document.dispatchEvent(new Event(CUSTOM_RENDER_EVENT));
   saveDataToStorage();
 }
 
@@ -73,9 +126,9 @@ function findBook(bookId) {
 }
 
 function findBookIndex(bookId) {
-  for (const bookIndex in books) {
-    if (bookIndex.id === bookId) {
-      return bookIndex;
+  for (const index in books) {
+    if (books[index].id === bookId) {
+      return index;
     }
   }
 
@@ -88,7 +141,7 @@ function addBookToCompleted(bookId) {
   if (bookTarget == null) return;
 
   bookTarget.isCompleted = true;
-  document.dispatchEvent(new Event(CUSTON_RENDER_EVENT));
+  document.dispatchEvent(new Event(CUSTOM_RENDER_EVENT));
   saveDataToStorage();
 }
 
@@ -98,77 +151,97 @@ function undoBookFromCompleted(bookId) {
   if (bookTarget == null) return;
 
   bookTarget.isCompleted = false;
-  document.dispatchEvent(new Event(CUSTON_RENDER_EVENT));
+  document.dispatchEvent(new Event(CUSTOM_RENDER_EVENT));
   saveDataToStorage();
 }
 
-function removeBookFromCompleted(bookId) {
+function removeBookBoth(bookId) {
   const bookTarget = findBookIndex(bookId);
-  
-  if (bookTarget == null) return;
-  
-  bookTarget.splice(bookTarget, 1);
-  document.dispatchEvent(new Event(CUSTON_RENDER_EVENT));
+
+  if (bookTarget === -1) return;
+
+  books.splice(bookTarget, 1);
+  document.dispatchEvent(new Event(CUSTOM_RENDER_EVENT));
   saveDataToStorage();
 }
 
+function makeBook(bookObject) {
+  const container = document.createElement("div");
+  container.setAttribute("id", `book-${bookObject.id}`);
 
+  const cardElement = `
+    <div class="card-data col-6">
+      <div class="data-text">
+        <div class="data-desc">
+          <h2>${bookObject.title}</h2>
+          <div class="data-sub-desc">
+            <h4>Author: </h4>
+            <span>${bookObject.author}</span>
+          </div>
+          <div class="data-sub-desc">
+            <h4>Year: </h4>
+            <span>${bookObject.year}</span>
+          </div>
+        </div>
+        <div class="data-sub-button">
+          <button class="button-checked">BLABLABLA</button>
+        </div>
+      </div>
+    </div>
+  `;
 
+  container.innerHTML += cardElement;
+  return container;
+}
 
+function loadDataFromStorage() {
+  const serializeData = localStorage.getItem(WEB_STORAGE_KEY);
+  const data = JSON.parse(serializeData);
 
-/**
- * Modal Section 
- */
-
-const modal = document.getElementById('myModalElement');
-const button = document.getElementById('myModalButton')
-const closeModal = document.getElementById('closeModal'); 
-
-// ketika user click button nya, maka modal akan terbuka
-button.addEventListener('click', () => {
-  modal.style.display = 'block';
-});
-
-// ketika user click pada closeModal, maka modal nya akan tertutup
-closeModal.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-// ketika user click dimanapun diluar modal, maka modal juga akan tertutup
-window.addEventListener('click', (event) => {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-});
-
-
-/**
- * Nav and Tabs section
- */
-function openTabs(evt, sectionName) {
-  let i;
-  const tabcontent = document.getElementsByClassName("tabcontent"); 
-  const tablinks = document.getElementsByClassName("tablinks"); 
-  const sectionTarget = document.getElementById(sectionName);
-
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  for (i = 0; i < btnChecker.length; i++) {
-    if (sectionName == 'finished') {
-      btnChecker[i].innerText = 'Mark NOT finished';
-    } else {
-      btnChecker[i].innerText = 'Mark finished';
+  if (data !== null) {
+    for (const book of data) {
+      books.push(book);
     }
   }
-  
-  sectionTarget.style.display = "flex";
-  evt.currentTarget.className += " active";
+
+  document.dispatchEvent(new Event(CUSTOM_RENDER_EVENT));
 }
 
+/**
+ * All Event Listener
+ *
+ */
 
+document.addEventListener("DOMContentLoaded", function () {
+  const myForm = document.getElementById("myForm");
+  myForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addBookList();
+  });
+
+  if (isWebStorageExist()) {
+    loadDataFromStorage();
+  }
+});
+
+document.addEventListener(CUSTOM_RENDER_EVENT, function () {
+  console.log(books);
+  const uncompletedBookList = document.getElementById("finished");
+  const completedBookList = document.getElementById("not-finished");
+
+  uncompletedBookList.innerText = "";
+  completedBookList.innerText = "";
+
+  for (const bookItem of books) {
+    const bookElement = makeBook(bookItem);
+    if (!bookItem.isCompleted) {
+      uncompletedBookList.appendChild(bookElement);
+    } else {
+      completedBookList.appendChild(bookElement);
+    }
+  }
+});
+
+document.addEventListener(CUSTOM_SAVED_EVENT, function () {
+  console.log("Data berhasil disimpan");
+});
